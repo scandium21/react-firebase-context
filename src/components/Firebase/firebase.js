5fd06a1e3680ed7519161040b8259a69e7b1daff
Siliang Chang <siliang.chang@gmail.com>
2020-03-18 22:25:08 -0600

    add firebase api methods

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -17,6 +17,22 @@ class Firebase {
   constructor() {
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
+    this.auth = firebase.auth();
   }
+
+  /** Auth API */
+  doCreateUserWithEmailAndPassword = (email, password) => {
+    this.auth.createUserWithEmailAndPassword(email, password);
+  };
+
+  doSignInWithEmailAndPassword = (email, password) => {
+    this.auth.signInWithEmailAndPassword(email, password);
+  };
+
+  doSignOut = () => this.auth.signOut();
+
+  doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+  doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
 }
 export default Firebase;
