cf7c1199c1ebadb2029886515b447ea8dda891ec
Siliang Chang <siliang.chang@gmail.com>
2020-03-19 22:12:06 -0600

    added sign up form input fields

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,7 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-const SignUp = props => {
-  return <div>SignUp</div>;
+import * as ROUTES from "../../constants/routes";
+
+const INITIAL_STATE = {
+  username: "",
+  email: "",
+  passwordOne: "",
+  passwordTwo: "",
+  error: null
 };
 
-export default SignUp;
+const SignUpPage = props => {
+  return (
+    <div>
+      <h1>SignUp</h1>
+      <SignUpForm />
+    </div>
+  );
+};
+
+const SignUpForm = props => {
+  const [state, setState] = useState(INITIAL_STATE);
+  const onSubmit = () => {};
+  const onChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+  const { username, email, passwordOne, passwordTwo, error } = state;
+  const isInvalid =
+    passwordOne !== passwordTwo ||
+    passwordOne === "" ||
+    email === "" ||
+    username === "";
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        name="username"
+        value={username}
+        onChange={onChange}
+        placeholder="Full name"
+      />
+      <input
+        type="text"
+        name="email"
+        value={email}
+        onChange={onChange}
+        placeholder="Email Address"
+      />
+      <input
+        type="password"
+        name="passwordOne"
+        value={passwordOne}
+        onChange={onChange}
+        placeholder="Password"
+      />
+      <input
+        type="password"
+        name="passwordTwo"
+        value={passwordTwo}
+        onChange={onChange}
+        placeholder="Confirm Password"
+      />
+      <button disabled={isInvalid} type="submit">
+        Sign Up
+      </button>
+      {error && <p>{error.message}</p>}
+    </form>
+  );
+};
+
+const SignUpLink = () => {
+  return (
+    <p>
+      Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+    </p>
+  );
+};
+
+export default SignUpPage;
+
+export { SignUpForm, SignUpLink };
