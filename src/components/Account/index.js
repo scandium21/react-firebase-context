253c27ff6bc69ab9d266e058767b15b6e0262458
Siliang Chang <siliang.chang@gmail.com>
2020-03-25 09:53:28 -0600

    added PasswordChange

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import { PasswordForgetForm } from "../PasswordForget";
+import PasswordChangeForm from "../PasswordChange";
+
 const Account = props => {
-  return <div>Account</div>;
+  return (
+    <div>
+      <h1>Account Page</h1>
+      <PasswordForgetForm />
+      <PasswordChangeForm />
+    </div>
+  );
 };
 
 export default Account;
--- src/components/PasswordChange/index.js
+++ src/components/PasswordChange/index.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-const PasswordChange = props => {
-  return <div>PasswordChange</div>;
+import { withFirebase } from "../Firebase";
+
+const INITIAL_STATE = {
+  passwordOne: "",
+  passwordTwo: "",
+  error: null
 };
 
-export default PasswordChange;
+const PasswordChangeForm = props => {
+  const [state, setState] = useState(INITIAL_STATE);
+  const { passwordOne, passwordTwo, error } = state;
+  const isInvalid = passwordOne !== passwordTwo || passwordOne === "";
+  const onSubmit = event => {};
+  const onChange = () => {};
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="password"
+        name="passwordOne"
+        value={passwordOne}
+        onChange={onChange}
+        placeholder="New Password"
+      />
+      <input
+        type="password"
+        name="passwordTwo"
+        value={passwordTwo}
+        onChange={onChange}
+        placeholder="Confirm New Password"
+      />
+      <button disabled={isInvalid} type="submit">
+        Reset My Password
+      </button>
+      {error && <p>{error.message}</p>}
+    </form>
+  );
+};
+
+export default withFirebase(PasswordChangeForm);
