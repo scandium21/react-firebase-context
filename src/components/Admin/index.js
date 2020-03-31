3e80c950258d893d923a91ae329ca73ed3dc725d
Siliang Chang <siliang.chang@gmail.com>
2020-03-30 23:19:35 -0600

    modify admin dashboard

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import { compose } from "recompose";
 
 import * as ROLES from "../../constants/roles";
+import * as ROUTES from "../../constants/routes";
 import {
   AuthUserContext,
   withAuthorization,
@@ -10,7 +12,69 @@ import {
 import { withFirebase } from "../Firebase";
 
 const Admin = props => {
+  return (
+    <AuthUserContext.Consumer>
+      {authUser => (
+        <div>
+          <h1>Admin</h1>
+          <p>Restricted area! Only users with the admin role are authorized.</p>
+          <Switch>
+            <Route exact path={ROUTES.ADMIN_DETAILS} component={UserItem} />
+            <Route exact path={ROUTES.ADMIN} component={UserList} />
+          </Switch>
+        </div>
+      )}
+    </AuthUserContext.Consumer>
+  );
+};
+
+const UserItemBase = props => {
+  const [state, setState] = useState({
+    loading: false,
+    user: null,
+    ...props.location.state
+  });
+  const { user, loading } = state;
+  useEffect(() => {
+    // If users navigate from the UserList to the UserItem component, should arrive immediately.
+    if (state.user) return;
+
+    // If URL is entered by hand or with other link, fetch user from db
+    setState({ ...state, loading: true });
+    props.firebase.user(props.match.params.id).on("value", snapshot => {
+      setState({
+        user: snapshot.val(),
+        loading: false
+      });
+    });
+    return function cleanup() {
+      props.firebase.user(props.match.params.id).off();
+    };
+  }, []);
+  return (
+    <div>
+      <h2>User ({props.match.params.id})</h2>
+      {loading && <div>Loading...</div>}
+      {user && (
+        <div>
+          <span>
+            <strong>ID:</strong> {user.uid}
+          </span>
+          <span>
+            <strong>Email:</strong> {user.email}
+          </span>
+          <span>
+            <strong>Username:</strong> {user.username}
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
+
+const UserListBase = props => {
   const [state, setState] = useState({ loading: false, users: [] });
+  const { loading, users } = state;
   useEffect(() => {
     setState({ ...state, loading: true });
     props.firebase.users().on("value", snapshot => {
@@ -28,43 +92,46 @@ const Admin = props => {
       };
     });
   }, []);
-  const { loading, users } = state;
   return (
-    <AuthUserContext.Consumer>
-      {authUser => (
-        <div>
-          <h1>Admin</h1>
-          <p>Restricted area! Only users with the admin role are authorized.</p>
-          {loading && <div>Loading...</div>}
-          <UserList users={users} />
-        </div>
-      )}
-    </AuthUserContext.Consumer>
+    <div>
+      <h2>Users</h2>
+      {loading && <div>Loading...</div>}
+      <ul>
+        {users.map(user => (
+          <li key={user.uid}>
+            <span>
+              <strong>ID: </strong>
+              {user.uid}
+            </span>
+            <span>
+              <strong>E-mail: </strong>
+              {user.email}
+            </span>
+            <span>
+              <strong>Username: </strong>
+              {user.username}
+            </span>
+            <span>
+              <Link
+                to={{
+                  pathname: `${ROUTES.ADMIN}/${user.uid}`,
+                  state: { user }
+                }}
+              >
+                Details
+              </Link>
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
-const UserList = ({ users }) => (
-  <ul>
-    {users.map(user => (
-      <li key={user.uid}>
-        <span>
-          <strong>ID: </strong>
-          {user.uid}
-        </span>
-        <span>
-          <strong>E-mail: </strong>
-          {user.email}
-        </span>
-        <span>
-          <strong>Username: </strong>
-          {user.username}
-        </span>
-      </li>
-    ))}
-  </ul>
-);
-
 const condition = authUser => authUser && !!authUser.roles[ROLES.ADMIN];
+
+const UserList = withFirebase(UserListBase);
+const UserItem = withFirebase(UserItemBase);
 
 export default compose(
   withEmailVerification,
